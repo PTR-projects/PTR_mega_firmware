a631d22b7cf48e39e6a63c6615877ce8bcc5c75a
Bartosz Dabrowski <bartosz.dabrowski.org@gmail.com>
2022-07-11 16:49:05 +0000

    *Added register definitions *Created basic calibration read *Added structs for data transfer +Weird data obtaining algorithm in sensor datsheet

--- components/MS5607_driver/MS5607_driver.c
+++ components/MS5607_driver/MS5607_driver.c
@@ -1,9 +1,69 @@
 #include <stdio.h>
 #include "esp_err.h"
+#include "Spi_driver.h"
 
 #include "MS5607_driver.h"
 
-esp_err_t MS5607_init()
-{
+#define slave SPI_SLAVE_MS5607
+
+MS5607_cal_t MS5607_cal_d;
+
+esp_err_t MS5607_init() {
 	return ESP_OK;	//ESP_FAIL
 }
+
+esp_err_t Ms5607_resetDevice() {
+
+	SPI_RW(slave, MS5607_Reset, NULL, 2); //trzeba dokonczyc, co zwraca reset? I w razie failu wywalic blad
+
+	return ESP_OK;
+}
+
+esp_err_t MS5607_readCalibration() {
+	MS5607_resetDevice();
+
+	uint8_t buf[15] = {0};
+	SPI_RW(SPI_SLAVE_MS5607, MS5607_Prom_Read, buf, 16);
+
+	MS5607_cal_d.C1 = *((uint16_t*)&buf[2]);
+	MS5607_cal_d.C2 = *((uint16_t*)&buf[4]);
+	MS5607_cal_d.C3 = *((uint16_t*)&buf[6]);
+	MS5607_cal_d.C4 = *((uint16_t*)&buf[8]);
+	MS5607_cal_d.C5 = *((uint16_t*)&buf[10]);
+	MS5607_cal_d.C6 = *((uint16_t*)&buf[12]);
+
+
+	return ESP_OK;
+
+
+	/*
+	 * 0 - > factory data
+	 * 1 - > C1
+	 * 2 - > C2
+	 * 3 - > C3
+	 * 4 - > C4
+	 * 5 - > C5
+	 * 6 - > C6
+	 * 7 - > Serial code + CRC
+	 *
+	 * 8 bytes - > 128 bits
+	 */
+}
+
+esp_err_t MS5607_startConv(char oversamplingRate) {
+	SPI_RW(SPI_SLAVE_MS5607, oversamplingRate, NULL, 2); //Dodac errorhandler
+
+	return ESP_OK;
+}
+
+esp_err_t MS5607_startMeas() {
+
+}
+
+void MS5607_readMeas(MS5607 * data) {
+	uint8_t = {0};
+	SPI_RW(SPI_SLAVE_MS5607, MS5607_Prom_Read, buf, 16);
+}
+
+
+
--- components/MS5607_driver/include/MS5607_driver.h
+++ components/MS5607_driver/include/MS5607_driver.h
@@ -2,7 +2,56 @@
 #include "esp_err.h"
 
 typedef struct {
+	uint32_t D1;
+	uint32_t D2;
 
+	int32_t temp_raw;
+	int32_t pres_raw;
+	float temp;
+	float press;
 } MS5607_t;
 
+typedef struct {
+	uint16_t C1;
+	uint16_t C2;
+	uint16_t C3;
+	uint16_t C4;
+	uint16_t C5;
+	uint16_t C6;
+} MS5607_cal_t;
+
+
 esp_err_t MS5607_init();
+esp_err_t Ms5607_resetDevice();
+esp_err_t MS5607_startConv(char oversamplingRate);
+esp_err_t MS5607_startMeas();
+void MS5607_readMeas(MS5607 * data);
+
+
+
+/*
+ * Registers
+ */
+#define MS5607_ADC_Read 		0x00
+#define MS5607_Prom_Read		0xA0
+#define MS5607_Reset 			0x1E
+
+
+/*
+ * Digital pressure oversampling settings
+ */
+#define MS5607_Convert_D1_256 	0x40
+#define MS5607_Convert_D1_512 	0x42
+#define MS5607_Convert_D1_1024 	0x44
+#define MS5607_Convert_D1_2048 	0x46
+#define MS5607_Convert_D1_4096	0x48
+
+
+/*
+ * Digital temperature oversampling settings
+ */
+#define MS5607_Convert_D2_256 	0x50
+#define MS5607_Convert_D2_512 	0x52
+#define MS5607_Convert_D2_1024 	0x54
+#define MS5607_Convert_D2_2048 	0x56
+#define MS5607_Convert_D2_4096	0x58
