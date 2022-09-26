a6af3c22dded30cef43293f699e0853b9fc2e1de
bartekM_home <space.tech@mail.com>
2022-09-26 22:45:07 +0200

    Added IGN driver from RECU

--- components/IGN_driver/include/IGN_driver.h
+++ components/IGN_driver/include/IGN_driver.h
@@ -1,1 +1,7 @@
+#pragma once
+
 esp_err_t IGN_init(void);
+esp_err_t IGN_srv(uint32_t time);
+uint8_t IGN_getState(uint8_t ign_no);
+esp_err_t IGN_set(uint8_t ign_no, uint8_t state);
+uint8_t IGN_check(uint8_t ign_no);
