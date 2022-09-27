08f0af61df04aef1c44822e89eae71c726353a58
bartekM_home <space.tech@mail.com>
2022-09-27 22:25:34 +0200

    LSM and LIS headers with data structure definitions added

--- components/LIS331_driver/include/LIS331_driver.h
+++ components/LIS331_driver/include/LIS331_driver.h
@@ -1,14 +1,42 @@
 #pragma once
 #include "esp_err.h"
 
-typedef enum{
-	LIS331_IC,
-	LIS331HH_IC,
-	H3LIS331_IC
-}LIS331_type_t;
-
 typedef struct{
+	float accX;
+	float accY;
+	float accZ;
+} LIS331_meas_t;
 
+typedef struct {
+	union{
+		uint8_t raw[10];
+		struct{
+			int16_t empty;		//0 1
+			int16_t accX_raw;	//2 3
+			int16_t accY_raw;	//4 5
+			int16_t accZ_raw;	//6 7
+		};
+	};
+
+	LIS331_meas_t meas;
+
+	float accXoffset;
+	float accYoffset;
+	float accZoffset;
+
+	float sensor_range;
 } LIS331_t;
+
+typedef enum{
+	LIS331_IC_2G 		= 2,
+	LIS331_IC_4G 		= 4,
+	LIS331_IC_8G 		= 8,
+	LIS331HH_IC_6G 		= 6,
+	LIS331HH_IC_12G 	= 12,
+	LIS331HH_IC_24G 	= 24,
+	H3LIS331_IC_100G 	= 100,
+	H3LIS331_IC_200G 	= 200,
+	H3LIS331_IC_400G 	= 400
+} LIS331_type_t;
 
 esp_err_t LIS331_init(LIS331_type_t type);
--- components/LSM6DSO32_driver/include/LSM6DSO32_driver.h
+++ components/LSM6DSO32_driver/include/LSM6DSO32_driver.h
@@ -1,3 +1,18 @@
+#pragma once
+
+
+typedef struct{
+	float temp;
+
+	float accX;
+	float accY;
+	float accZ;
+
+	float gyroX;
+	float gyroY;
+	float gyroZ;
+} LSM6DS_meas_t;
+
 typedef struct{
 
 } LSM6DSO32_t;
