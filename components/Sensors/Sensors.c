1f35227b096590c5486a27769bea6d833ccb013e
bartekM_home <space.tech@mail.com>
2022-09-27 22:32:59 +0200

    Sensors component updated
    Init functions added
    Meas Read and Get functions added

--- components/Sensors/Sensors.c
+++ components/Sensors/Sensors.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
+#include "MS5607_driver.h"
+#include "LIS331_driver.h"
+#include "MMC5983MA_driver.h"
+#include "LSM6DSO32_driver.h"
 
 #include "Sensors.h"
+
+esp_err_t Sensors_axes_translation();
 
 //--------- Private var ---------------
 static Sensors_t Sensors_d;
@@ -11,11 +16,53 @@ esp_err_t Sensors_init()
 {
 	memset(&Sensors_d, 0, sizeof(Sensors_d));
 
+	MS5607_init();
+//	LIS331_init(H3LIS331_IC_100G);
+	MMC5983MA_init();
+//	LSM6DSO32_init();
+
 	return ESP_OK; 	//ESP_FAIL
 }
 
 esp_err_t Sensors_update(){
 	//get new data from sensors
 
+	MS5607_getReloadSmart();
+//	LIS331_readMeas();
+//	LSM6DSO32_readMeas();
+	MMC5983MA_readMeas();
+
+	MS5607_getMeas(&(Sensors_d.MS5607));
+//	LIS331_getMeas(&(Sensors_d.LIS331));
+//	LSM6DSO32_getMeas(&(Sensors_d.LSM6DSO32));
+	MMC5983MA_getMeas(&(Sensors_d.MMC5983MA));
+
+	Sensors_axes_translation();
+
 	return ESP_OK; 	//ESP_FAIL
+}
+
+esp_err_t Sensors_axes_translation(){
+	Sensors_t Sensors_b = Sensors_d;
+
+	Sensors_d.LIS331.accY     = -Sensors_b.LIS331.accY;
+	Sensors_d.LIS331.accZ     = -Sensors_b.LIS331.accZ;
+
+	Sensors_d.LSM6DSO32.accX  =  Sensors_b.LSM6DSO32.accY;
+	Sensors_d.LSM6DSO32.accY  =  Sensors_b.LSM6DSO32.accX;
+	Sensors_d.LSM6DSO32.accZ  = -Sensors_b.LSM6DSO32.accZ;
+
+	Sensors_d.LSM6DSO32.gyroX =  Sensors_b.LSM6DSO32.gyroY;
+	Sensors_d.LSM6DSO32.gyroY =  Sensors_b.LSM6DSO32.gyroX;
+	Sensors_d.LSM6DSO32.gyroZ = -Sensors_b.LSM6DSO32.gyroZ;
+
+	Sensors_d.MMC5983MA.magX  = -Sensors_b.MMC5983MA.magY;
+	Sensors_d.MMC5983MA.magY  = -Sensors_b.MMC5983MA.magX;
+	Sensors_d.MMC5983MA.magZ  =  Sensors_b.MMC5983MA.magZ;
+
+	return ESP_OK;
+}
+
+Sensors_t * Sensors_get(){
+	return &Sensors_d;
 }
--- components/Sensors/include/Sensors.h
+++ components/Sensors/include/Sensors.h
@@ -6,11 +6,12 @@
 #include "MS5607_driver.h"
 
 typedef struct{
-	LIS331_t 	LIS331;
-	LSM6DSO32_t LSM6DSO32;
-	MMC5983MA_t MMC5983MA;
-	MS5607_t 	MS5607;
+	LIS331_meas_t 		LIS331;
+	LSM6DS_meas_t 		LSM6DSO32;
+	MMC5983MA_meas_t 	MMC5983MA;
+	MS5607_meas_t 		MS5607;
 } Sensors_t;
 
 esp_err_t Sensors_init();
 esp_err_t Sensors_update();
+Sensors_t * Sensors_get();
