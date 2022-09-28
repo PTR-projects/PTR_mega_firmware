75f33fe216a430d62a4830e2ede68e540131d12f
Bartosz Dabrowski <bartosz.dabrowski.org@gmail.com>
2022-09-28 17:04:01 +0000

    Added storage skeleton -not verified

---
+++ components/Storage_driver/Storage_driver.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_err.h"
+
+#include "Storage_driver.h"
+
+uint32_t MasterKey;
+
+esp_err_t Storage_init(Storage_filesystem_t FS, uint32_t key){
+
+	MasterKey = key;
+	return ESP_OK;
+}
+esp_err_t Storage_erase(uint32_t key){
+	if(key != MasterKey){
+		return ESP_FAIL;
+	}
+	return ESP_OK;
+}
+
+esp_err_t Storage_writePacket(void * data, uint16_t length){
+	return ESP_OK;
+
+}
+esp_err_t Storage_readFile(){
+	return ESP_OK;
+}
+esp_err_t Storage_readAll(){	
+	return ESP_OK;
+}
+
+uint16_t Storage_getFreeMem(void){
+	
+}
+
+
+
+
+
---
+++ components/Storage_driver/include/Storage_driver.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "esp_err.h"
+
+typedef enum{
+	FILESYSTEM_LITTLEFS = 1,
+	FILESYSTEM_SPIFFS = 2
+} Storage_filesystem_t; 
+
+
+esp_err_t Storage_init(Storage_filesystem_t FS, uint32_t key);
+esp_err_t Storage_erase(uint32_t key);
+
+esp_err_t Storage_writePacket(void * data, uint16_t length);
+esp_err_t Storage_readFile();
+esp_err_t Storage_readAll();
+
+uint16_t Storage_getFreeMem(void);
+
+
+
