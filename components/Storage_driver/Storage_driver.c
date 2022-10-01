f134120914431b50ae5f0c963763a187eaf1a21c
Bartosz Dabrowski <bartosz.dabrowski.org@gmail.com>
2022-10-01 17:18:10 +0000

    Dalej buduję dodałem littlefs, sprawdzanie ilosci plikow

--- components/Storage_driver/Storage_driver.c
+++ components/Storage_driver/Storage_driver.c
@@ -5,54 +5,61 @@
 
 #include "Storage_driver.h"
 
-uint32_t MasterKey;
+static Storage_data_t Storage_data_d;
 
 esp_vfs_spiffs_conf_t conf = {
       .base_path = "/spiffs",
       .partition_label = NULL,
       .max_files = 5,
-      .format_if_mount_failed = true
+      .format_if_mount_failed = false
     };
 
 
 esp_err_t Storage_init(Storage_filesystem_t FS, uint32_t key){
 
-	MasterKey = key;
+    uint8_t fileCnt = 0;
+	Storage_data_d.MasterKey = key;
 	
-	esp_err_t ret = esp_vfs_spiffs_register(&conf);
-
-    if (ret != ESP_OK) {
-        if (ret == ESP_FAIL) {
-           
-        } else if (ret == ESP_ERR_NOT_FOUND) {
-            
-        } else {
-           
-        }
-       
+    struct stat st;
+    while(stat("/spiffs/foo.txt", &st)){
+        fileCnt++;
     }
+
+    Storage_data_d.fileCount = fileCnt;
+
+    
+
+
 	return ESP_OK;
 }
+
 esp_err_t Storage_erase(uint32_t key){
-	if(key != MasterKey){
+	if(key != Storage_data_d.MasterKey){
 		return ESP_FAIL;
 	}
 	return ESP_OK;
 }
 
-esp_err_t Storage_writePacket(void * data, uint16_t length){
+esp_err_t Storage_writePacket_Spiffs(void * data, uint16_t length){
 	return ESP_OK;
 
 }
-esp_err_t Storage_readFile(){
+esp_err_t Storage_readFile_Spiffs(){
 	return ESP_OK;
 }
+
+
 esp_err_t Storage_readAll(){	
 	return ESP_OK;
 }
 
-uint16_t Storage_getFreeMem(void){
-	return 0;
+uint16_t Storage_getFreeMem_Spiffs(void){
+    char partition_label;
+    uint16_t total_bytes, used_bytes;
+
+    esp_err_t esp_spiffs_info(partition_label,  total_bytes,  used_bytes);
+
+	return total_bytes-used_bytes;
 	
 }
 
--- components/Storage_driver/include/Storage_driver.h
+++ components/Storage_driver/include/Storage_driver.h
@@ -1,12 +1,18 @@
 #pragma once
 #include "esp_err.h"
 #include "esp_spiffs.h"
+#include "esp_littlefs.h"
+#include "esp_flash.h"
 
 typedef enum{
 	FILESYSTEM_LITTLEFS = 1,
 	FILESYSTEM_SPIFFS = 2
 } Storage_filesystem_t; 
 
+typedef struct{
+	uint32_t MasterKey;
+	uint8_t fileCount;
+} Storage_data_t;
 
 
 esp_err_t Storage_init(Storage_filesystem_t FS, uint32_t key);
@@ -14,9 +20,19 @@ esp_err_t Storage_erase(uint32_t key);
 
 esp_err_t Storage_writePacket(void * data, uint16_t length);
 esp_err_t Storage_readFile();
+
+
+esp_err_t Storage_writePacket_Spiffs(void * data, uint16_t length);
+esp_err_t Storage_readFile_Spiffs();
+uint16_t Storage_getFreeMem_Spiffs(void);
+
+esp_err_t Storage_writePacket_Littlefs(void * data, uint16_t length);
+esp_err_t Storage_readFile_Littlefs();
+uint16_t Storage_getFreeMem_Littlefs(void);
+
 esp_err_t Storage_readAll();
 
-uint16_t Storage_getFreeMem(void);
+
 
 
 
