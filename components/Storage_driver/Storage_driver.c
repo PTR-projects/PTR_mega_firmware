6b5a3aa7ed5668bfc57a95f842289e1c4f018193
Bartosz Dabrowski <bartosz.dabrowski.org@gmail.com>
2022-11-19 14:43:37 +0000

    Re-written component to suit single file approach To check: * max file size for init -> not sure if it works correctly due to idf bug with resetting eps multiple times

--- components/Storage_driver/Storage_driver.c
+++ components/Storage_driver/Storage_driver.c
@@ -4,11 +4,24 @@
 #include <string.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/semphr.h"
 #include "esp_err.h"
 #include "esp_log.h"
-
-
 #include "Storage_driver.h"
+
+esp_err_t Storage_init_Spiffs			();
+esp_err_t Storage_init_Littlefs			();
+esp_err_t Storage_createNextFile		();
+esp_err_t Storage_erase_Spiffs			(uint32_t key);
+esp_err_t Storage_erase_Littlefs		(uint32_t key);
+esp_err_t Storage_writePacket_Spiffs	(void * buf, uint16_t len);
+esp_err_t Storage_writePacket_Littlefs	(void * buf, uint16_t len);
+esp_err_t Storage_readFile_Spiffs		(void * buf);
+esp_err_t Storage_readFile_Littlefs		(void * buf);
+size_t    Storage_getFreeMem_Spiffs		(void);
+size_t    Storage_getFreeMem_Littlefs	(void);
+
+
 
 static Storage_data_t Storage_data_d;
 
@@ -26,107 +39,129 @@ esp_vfs_littlefs_conf_t conf_littlefs = {
     };
 
 static const char *TAG = "Storage_driver";
-//######################################################################################################################
-//													INITIALIZING FILES
-//######################################################################################################################
 
+
+/*!
+ * @brief Initialize storage component by calling init functions for specified filesystem.
+ * @param filesystem
+ * @param key
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_init(Storage_filesystem_t filesys , uint32_t key){
 
+	esp_err_t ret=ESP_FAIL;
     Storage_data_d.Storage_filestystem_d = filesys;
-
-    vTaskDelay(pdMS_TO_TICKS( 1000 ));
+    Storage_data_d.flag=false;
+    Storage_data_d.MasterKey = key;
 
     if(filesys == 1){
-        return Storage_init_Spiffs(key);
+        ret = Storage_init_Spiffs(key);
     }
-    else if(filesys == 2)
-    {
-        return Storage_init_Littlefs(key);
+    else if(filesys == 2){
+        ret = Storage_init_Littlefs(key);
     }
 
-    return ESP_FAIL;
+
+    if(ret==ESP_OK){
+    	Storage_data_d.flag=true;
+    }
+    return ret;
 }
 
-esp_err_t Storage_init_Spiffs(uint32_t key){
-
-    uint8_t fileCnt = 0;
-	Storage_data_d.MasterKey = key;
+/*!
+ * @brief Initialize spiffs filesystem and check if the log file file can be created.
+ * @return Esp_err_t.
+ */
+esp_err_t Storage_init_Spiffs(){
+	strcpy(Storage_data_d.path, "/spiffs/meas.bin");
 
     esp_err_t ret = esp_vfs_spiffs_register(&conf_spiffs);
     if(ret != ESP_OK){
         return ESP_FAIL;
     }
 
-    //Count number of existing measurment files
-    struct stat st;
-    char path[20];
-    sprintf(path, "/spiffs/Meas%02i.csv", fileCnt);
-
-    while(!stat(path, &st)){
-        fileCnt++;
-        sprintf(path, "/spiffs/Meas%02i.csv", fileCnt);
-    }
-
-    Storage_data_d.fileCount = fileCnt;
+    ret = Storage_createNextFile();
 
     size_t freeSpace = Storage_getFreeMem_Spiffs();
+
     if(freeSpace < Storage_data_d.minFreeMem){
-        return ESP_ERR_NO_MEM;
+    	ret = ESP_FAIL;
     }
 
-    strncpy(Storage_data_d.path, path, 20);
 
-    //Add file with index fileCount and write config data
-    FILE* f = fopen(Storage_data_d.path, "w");
-    //tutaj ma być funkcja dodająca naglowek pliku typu nazwy kolumn, konfiguracja itd.
-    fclose(f);
-
-	return ESP_OK;
+	return ret;
 }
 
-esp_err_t Storage_init_Littlefs(uint32_t key){
+/*!
+ * @brief Initialize littlefs filesystem and check if the log file file can be created.
+ * @return Esp_err_t.
+ */
+esp_err_t Storage_init_Littlefs(){
 
-    uint8_t fileCnt = 0;
-	Storage_data_d.MasterKey = key;
+	strcpy(Storage_data_d.path, "/littlefs/meas.bin");
 
 	esp_err_t ret = esp_vfs_littlefs_register(&conf_littlefs);
     if(ret != ESP_OK){
         return ESP_FAIL;
     }
 
-    //Count number of existing measurment files
-    struct stat st;
-    char path[22];
-    sprintf(path, "/littlefs/Meas%02i.csv", fileCnt);
-
-    while(!stat(path, &st)){
-        fileCnt++;
-        sprintf(path, "/littlefs/Meas%02i.csv", fileCnt);
-    }
-
-    Storage_data_d.fileCount = fileCnt;
+    ret = Storage_createNextFile();
 
     size_t freeSpace = Storage_getFreeMem_Littlefs();
-    if(freeSpace < Storage_data_d.minFreeMem){
-    	return ESP_ERR_NO_MEM;
-    }
 
-    strncpy(Storage_data_d.path, path, 22);
+	if(freeSpace < Storage_data_d.minFreeMem){
+		return ESP_FAIL;
+	}
 
-    //Add file with index fileCount and write config data
 
-    FILE* f = fopen(Storage_data_d.path, "w");
-    //tutaj ma być funkcja dodająca naglowek pliku typu nazwy kolumn, konfiguracja itd.
-    fclose(f);
+	return ret;
+}
+
+/*!
+ * @brief Check if new file can be created
+ * @return Esp_err_t.
+ */
+esp_err_t Storage_createNextFile(){
+
+	struct stat st;
+
+
+	if(stat(Storage_data_d.path, &st)){
+		FILE* f = fopen(Storage_data_d.path, "w");
+		ESP_LOGE(TAG, "SS: %ld", st.st_size);
+		fclose(f);
+	}
+	else if(!stat(Storage_data_d.path, &st) && st.st_size>20){//do sprawdzenia wartoś pamięci minimalnej
+		ESP_LOGE(TAG, "Szie: %ld", st.st_size);
+		return ESP_FAIL;
+	}
+	else{
+		ESP_LOGE(TAG, "ie: %ld", st.st_size);
+		Storage_erase(Storage_data_d.MasterKey);
+		FILE* f = fopen(Storage_data_d.path, "w");
+		ESP_LOGI(TAG, "SS: %ld", st.st_size);
+		fclose(f);
+	}
+
+
+
 
 	return ESP_OK;
 }
-
 //######################################################################################################################
 //													CLEARING FILES
 //######################################################################################################################
 
+/*!
+ * @brief Erase memory by calling filesystem specific function
+ * @param key
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_erase(uint32_t key){
+
+	if(!Storage_data_d.flag){
+		return ESP_FAIL;
+	}
 
 	if(Storage_data_d.Storage_filestystem_d == 1){
         return Storage_erase_Spiffs(key);
@@ -138,9 +173,12 @@ esp_err_t Storage_erase(uint32_t key){
     return ESP_FAIL;
 }
 
+
+
 esp_err_t Storage_erase_Spiffs(uint32_t key){
 	if(key != Storage_data_d.MasterKey){
-		return ESP_ERR_INVALID_ARG;
+		ESP_LOGE(TAG, "Erase - wrong key");
+		return ESP_FAIL;
 	}
 
     return esp_spiffs_format(conf_spiffs.partition_label);
@@ -148,7 +186,8 @@ esp_err_t Storage_erase_Spiffs(uint32_t key){
 
 esp_err_t Storage_erase_Littlefs(uint32_t key){
 	if(key != Storage_data_d.MasterKey){
-		return ESP_ERR_INVALID_ARG;
+		ESP_LOGE(TAG, "Erase - wrong key");
+		return ESP_FAIL;
 	}
 
     return esp_littlefs_format(conf_littlefs.partition_label);
@@ -159,24 +198,42 @@ esp_err_t Storage_erase_Littlefs(uint32_t key){
 //													WRITING FILES
 //######################################################################################################################
 
+/*!
+ * @brief Write packet of given size by calling filesystem specific function
+ * @param buff pointer to a buffer
+ * @param len length of buffer in Bytes
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_writePacket(void * buf, uint16_t len){
 
-    if(Storage_data_d.Storage_filestystem_d == 1){
-        return Storage_writePacket_Spiffs(buf, len);
-    }
-    else if(Storage_data_d.Storage_filestystem_d == 2){
-    	return Storage_writePacket_Littlefs(buf, len);
-    }
+	esp_err_t res = ESP_FAIL;
 
-	return ESP_FAIL;
+	if(!Storage_data_d.flag){
+		return ESP_FAIL;
+	}
+
+	if(Storage_data_d.Storage_filestystem_d == 1){
+		res = Storage_writePacket_Spiffs(buf, len);
+	}
+	else if(Storage_data_d.Storage_filestystem_d == 2){
+		res = Storage_writePacket_Littlefs(buf, len);
+	}
+
+	return res;
 }
 
+/*!
+ * @brief Write packet of given size in spiffs
+ * @param buff pointer to a buffer
+ * @param len length of buffer in Bytes
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_writePacket_Spiffs(void * buf, uint16_t len){
 
     FILE* f = fopen(Storage_data_d.path, "a");
 
     if(f == NULL){
-        return ESP_ERR_NOT_FOUND;
+        return ESP_FAIL;
     }
 
     fwrite(buf, len, 1, f);
@@ -186,12 +243,18 @@ esp_err_t Storage_writePacket_Spiffs(void * buf, uint16_t len){
 	return ESP_OK;
 }
 
+/*!
+ * @brief Write packet of given size in littlefs
+ * @param buff pointer to a buffer
+ * @param len length of buffer in Bytes
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_writePacket_Littlefs(void * buf, uint16_t len){
-
     FILE* f = fopen(Storage_data_d.path, "a");
 
     if(f == NULL){
-    	return ESP_ERR_NOT_FOUND;
+        //ESP_LOGE(TAG, "Failed to open file for reading");
+        return ESP_FAIL;
     }
 
     fwrite(buf, len, 1, f);
@@ -201,11 +264,18 @@ esp_err_t Storage_writePacket_Littlefs(void * buf, uint16_t len){
 	return ESP_OK;
 }
 
-//######################################################################################################################
-//													READING FILES
-//######################################################################################################################
 
+/*!
+ * @brief Read whole file by calling filesystem specific function
+ * @param buff pointer to an output buffer
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_readFile(void * buf){
+
+	if(!Storage_data_d.flag){
+		return ESP_FAIL;
+	}
+
 	if(Storage_data_d.Storage_filestystem_d == 1){
 		return Storage_readFile_Spiffs(buf);
 	}
@@ -217,6 +287,12 @@ esp_err_t Storage_readFile(void * buf){
 }
 
 
+
+/*!
+ * @brief Read whole file from spiffs
+ * @param buff pointer to an output buffer
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_readFile_Spiffs(void * buf){
 
     FILE* f = fopen(Storage_data_d.path, "r");
@@ -236,6 +312,11 @@ esp_err_t Storage_readFile_Spiffs(void * buf){
 	return ESP_OK;
 }
 
+/*!
+ * @brief Read whole file from littlefs
+ * @param buff pointer to an output buffer
+ * @return Esp_err_t.
+ */
 esp_err_t Storage_readFile_Littlefs(void * buf){
 	 FILE* f = fopen(Storage_data_d.path, "r");
 
@@ -257,15 +338,15 @@ esp_err_t Storage_readFile_Littlefs(void * buf){
 
 
 
-esp_err_t Storage_readAll(){
-	return ESP_OK;
-}
-
 
 //######################################################################################################################
 //													GETTING FREE SPACE
 //######################################################################################################################
 
+/*!
+ * @brief Get amount of free memory space by calling filesystem specific function
+ * @return size_t amount of free memory available
+ */
 size_t Storage_getFreeMem()
 {
      if(Storage_data_d.Storage_filestystem_d == 1){
@@ -278,80 +359,43 @@ size_t Storage_getFreeMem()
      return ESP_FAIL;
 }
 
+/*!
+ * @brief Get amount of free memory from spiifs
+ * @return size_t amount of free memory available
+ */
 size_t Storage_getFreeMem_Spiffs(void){
     size_t total_bytes = 0, used_bytes = 0;
 
     esp_spiffs_info(conf_spiffs.partition_label,  &total_bytes,  &used_bytes);
 
 	return (total_bytes-used_bytes)/1000;
-
 }
 
-
+/*!
+ * @brief Get amount of free memory from littlefs
+ * @return size_t amount of free memory available
+ */
 size_t Storage_getFreeMem_Littlefs(void){
     size_t total_bytes = 0, used_bytes = 0;
 
-    esp_err_t ret = esp_littlefs_info(conf_littlefs.partition_label,  &total_bytes,  &used_bytes);
-
-    if(ret == ESP_ERR_INVALID_STATE){
-    	return ESP_ERR_INVALID_STATE;
-    }
+    esp_littlefs_info(conf_littlefs.partition_label,  &total_bytes,  &used_bytes);
 
 	return (total_bytes-used_bytes)/1000;
-
 }
-
 
 
 //######################################################################################################################
 //													UTILITY FUNCTIONS
 //######################################################################################################################
 
+
+
+/*!
+ * @brief Retrieve Storage_driver configuration files
+ * @return storage_data_t struct with configuration parameters
+ */
 Storage_data_t Storage_listParams(void){
 	return Storage_data_d;
 }
-
-esp_err_t Storage_listFiles(void){
-
-	 uint8_t fileCnt = 0;
-	 struct stat st;
-	 char path[22], filename[12];
-
-	 sprintf(path, "/littlefs/Meas%02i.csv", fileCnt);
-	 sprintf(filename, "Meas%02i.csv", fileCnt);
-
-
-	 while(stat(path, &st) == 0){
-
-		 FILE* f = fopen(path, "r");
-
-		 if(f == NULL){
-			 return ESP_ERR_NOT_FOUND;
-		 }
-
-
-		 fseek(f, 0L, SEEK_END);
-		 size_t size = ftell(f);
-		 fseek(f, 0L, SEEK_SET);
-		 files[fileCnt].size = size/1000;
-		 fclose(f);
-
-
-		 //ESP_LOGI(TAG, "Size: %s, %d Bytes", filename, size);
-
-		 strncpy(files[fileCnt].filename, filename, 12);
-
-		 fileCnt++;
-		 sprintf(path, "/littlefs/Meas%02i.csv", fileCnt);
-		 sprintf(filename, "Meas%02i.csv", fileCnt);
-
-	 }
-
-
-	 return ESP_OK;
-
-}
-
-
 
 
--- components/Storage_driver/include/Storage_driver.h
+++ components/Storage_driver/include/Storage_driver.h
@@ -9,22 +9,18 @@ typedef enum{
     Storage_filesystem_littlefs = 2
 } Storage_filesystem_t;
 
-
 typedef struct{
 	Storage_filesystem_t Storage_filestystem_d;
 	uint32_t MasterKey;
 	uint8_t fileCount;
 	size_t minFreeMem;
-	char path[22];
+	char path[20];
+	bool flag;
+
 } Storage_data_t;
 
-typedef struct{
-    char filename[12];
-    uint32_t start_pos;
-    size_t size; //in kB
-} file_stat_t;
 
-file_stat_t files[256];
+
 
 
 
@@ -33,26 +29,7 @@ esp_err_t Storage_erase(uint32_t key);
 esp_err_t Storage_writePacket(void * buf, uint16_t len);
 esp_err_t Storage_readFile(void * buf);
 size_t Storage_getFreeMem(void);
-esp_err_t Storage_readAll();
 
-
-esp_err_t Storage_init_Spiffs(uint32_t key);
-esp_err_t Storage_init_Littlefs(uint32_t key);
-
-esp_err_t Storage_erase_Spiffs(uint32_t key);
-esp_err_t Storage_erase_Littlefs(uint32_t key);
-
-esp_err_t Storage_writePacket_Spiffs(void * buf, uint16_t len);
-esp_err_t Storage_writePacket_Littlefs(void * buf, uint16_t len);
-
-esp_err_t Storage_readFile_Spiffs(void * buf);
-esp_err_t Storage_readFile_Littlefs(void * buf);
-
-size_t Storage_getFreeMem_Spiffs(void);
-size_t Storage_getFreeMem_Littlefs(void);
 
 Storage_data_t Storage_listParams(void);
-esp_err_t Storage_listFiles(void);
-
-
 
