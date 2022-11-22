6c7b579ceb2d5e05569a71a68431923cf91d4aa9
bartekM_home <space.tech@mail.com>
2022-11-22 22:31:00 +0100

    State detector filled with functions. It should work once work on AHRS is complete.

--- components/FlightStateDetector/FlightStateDetector.c
+++ components/FlightStateDetector/FlightStateDetector.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include "esp_log.h"
 #include "esp_err.h"
 #include "AHRS_driver.h"
 #include "FlightStateDetector.h"
+
+#define TIME_ELAPSED(start_ms, now_ms, wait_ms) (start_ms <= (now_ms - wait_ms))
 
 //------ Private fun -----
 static void FlightState_STARTUP				(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
@@ -11,12 +14,8 @@ static void FlightState_PREFLIGHT_ERROR		(uint64_t time_ms, FlightState_t * curr
 static void FlightState_ME_ACCELERATING		(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
 static void FlightState_ME_ERROR			(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
 static void FlightState_FREEFLIGHT			(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
-static void FlightState_APOGEE				(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
 static void FlightState_FREEFALL			(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
-static void FlightState_DRAGCHUTE_DEPLOY	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
 static void FlightState_DRAGCHUTE_FALL		(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
-static void FlightState_DRAGCHUTE_ERROR		(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
-static void FlightState_MAINSHUTE_DEPLOY	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
 static void FlightState_MAINSHUTE_FALL		(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
 static void FlightState_LANDING				(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs);
 
@@ -25,8 +24,8 @@ static FlightState_t flightState_d;
 static AHRS_t * 	 AHRS_ptr;
 static const char *TAG = "FSD";
 
-uint32_t fsdEventTime;
-armingstatus_t armstatus_d = DISARMED;
+static uint64_t stateChangeTime = 0;
+static armingstatus_t armstatus_d = DISARMED;
 
 void FSD_arming(){
 	armstatus_d = ARMED;
@@ -92,28 +91,12 @@ esp_err_t FSD_detect(uint64_t time_ms){
 		FlightState_FREEFLIGHT(time_ms, currentState, ahrs);
 		break;
 
-	case FLIGHTSTATE_APOGEE:
-		FlightState_APOGEE(time_ms, currentState, ahrs);
-		break;
-
 	case FLIGHTSTATE_FREEFALL:
 		FlightState_FREEFALL(time_ms, currentState, ahrs);
 		break;
 
-	case FLIGHTSTATE_DRAGCHUTE_DEPLOY:
-		FlightState_DRAGCHUTE_DEPLOY(time_ms, currentState, ahrs);
-		break;
-
 	case FLIGHTSTATE_DRAGCHUTE_FALL:
 		FlightState_DRAGCHUTE_FALL(time_ms, currentState, ahrs);
-		break;
-
-	case FLIGHTSTATE_DRAGCHUTE_ERROR:
-		FlightState_DRAGCHUTE_ERROR(time_ms, currentState, ahrs);
-		break;
-
-	case FLIGHTSTATE_MAINSHUTE_DEPLOY:
-		FlightState_MAINSHUTE_DEPLOY(time_ms, currentState, ahrs);
 		break;
 
 	case FLIGHTSTATE_MAINSHUTE_FALL:
@@ -131,3 +114,192 @@ esp_err_t FSD_detect(uint64_t time_ms){
 
 	return ESP_OK;
 }
+
+//----------- Private functions ----------------------------------
+static void FlightState_STARTUP	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
+	//------ Komendy wykonywane co pętlę ------
+
+
+	//------ Warunki przejścia dalej ------
+	if ((TIME_ELAPSED(stateChangeTime, time_ms, 5000)) /* && (SysManager_checkStartupDone() == ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_PREFLIGHT;
+		currentState->state_ready = false;
+	}
+
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkStartupDone() != ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_PREFLIGHT_ERROR;
+		currentState->state_ready = false;
+	}
+}
+
+static void FlightState_PREFLIGHT (uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
+	//------ Komendy wykonywane co pętlę ------
+
+
+	//------ Warunki przejścia dalej ------
+	if ((TIME_ELAPSED(stateChangeTime, time_ms, 100)) && (ahrs->acc_axis_lowpass >= 2.0f) ) {
+		currentState->state = FLIGHTSTATE_ME_ACCELERATING;
+		currentState->state_ready = false;
+	}
+
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_PREFLIGHT_ERROR;
+		currentState->state_ready = false;
+	}
+}
+
+static void FlightState_PREFLIGHT_ERROR	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+
+}
+
+static void FlightState_ME_ACCELERATING	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
+	//------ Komendy wykonywane co pętlę ------
+
+
+	//------ Warunki przejścia dalej ------
+	if ((TIME_ELAPSED(stateChangeTime, time_ms, 200))  && (ahrs->acc_axis_lowpass < 0.0f) ) {
+		currentState->state = FLIGHTSTATE_FREEFLIGHT;
+		currentState->state_ready = false;
+	}
+
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_ME_ERROR;
+		currentState->state_ready = false;
+	}
+}
+
+static void FlightState_ME_ERROR	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+
+}
+
+static void FlightState_FREEFLIGHT			(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
+	//------ Komendy wykonywane co pętlę ------
+
+
+	//------ Warunki przejścia dalej ------
+	if ((TIME_ELAPSED(stateChangeTime, time_ms, 200))  && ((ahrs->max_altitude - ahrs->pos_ef.z) > 10.0f) ) {
+		currentState->state = FLIGHTSTATE_FREEFALL;
+		currentState->state_ready = false;
+	}
+
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_FREEFALL;
+		currentState->state_ready = false;
+	}
+}
+
+static void FlightState_FREEFALL (uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
+	//------ Komendy wykonywane co pętlę ------
+
+
+	//------ Warunki przejścia dalej ------
+	if ((TIME_ELAPSED(stateChangeTime, time_ms, 100)) && (1)) {
+		currentState->state = FLIGHTSTATE_DRAGCHUTE_FALL;
+		currentState->state_ready = false;
+	}
+
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_DRAGCHUTE_FALL;
+		currentState->state_ready = false;
+	}
+}
+
+static void FlightState_DRAGCHUTE_FALL (uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
+	//------ Komendy wykonywane co pętlę ------
+
+
+	//------ Warunki przejścia dalej ------
+	if ((TIME_ELAPSED(stateChangeTime, time_ms, 100) && (ahrs->pos_ef.z < 200.0f))
+		|| (TIME_ELAPSED(stateChangeTime, time_ms, 2000) && (ahrs->vel_ef.z < -60.0f)) ) {
+		currentState->state = FLIGHTSTATE_MAINSHUTE_FALL;
+		currentState->state_ready = false;
+	}
+
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_MAINSHUTE_FALL;
+		currentState->state_ready = false;
+	}
+}
+
+static void FlightState_MAINSHUTE_FALL (uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
+	//------ Komendy wykonywane co pętlę ------
+
+
+	//------ Warunki przejścia dalej ------
+	if (TIME_ELAPSED(stateChangeTime, time_ms, 10000) /* && (ahrs->pos.z < 200.0f) && (ahrs->vel.z > -1.0f)) */){
+		currentState->state = FLIGHTSTATE_LANDING;
+		currentState->state_ready = false;
+	}
+
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
+		currentState->state = FLIGHTSTATE_LANDING;
+		currentState->state_ready = false;
+	}
+}
+
+static void FlightState_LANDING	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane co pętlę ------
+
+	//------ Warunki wykrycia awarii ------
+	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
+		//shut down safely
+	}
+}
+
+
