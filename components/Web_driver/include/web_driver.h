40a1af546d4d80eb3810637ef9bc5ab7cdefe9a1
Bartosz Dabrowski <bartosz.dabrowski.org@gmail.com>
2022-11-25 12:25:09 +0000

    Features added + Added upload, download and delet of any file + basic parser for some data types

--- components/Web_driver/include/web_driver.h
+++ components/Web_driver/include/web_driver.h
@@ -1,14 +1,23 @@
 #pragma once
+
 #include "esp_err.h"
+#include "esp_http_server.h"
+#include "esp_vfs.h"
 
 
-typedef struct{
-	char password[20];
-	char ssid[20];
-} Web_data_t;
+/* Scratch buffer size */
+#define SCRATCH_BUFSIZE  8192
 
+struct file_server_data {
+    /* Base path of file storage */
+    char base_path[ESP_VFS_PATH_MAX + 1];
+
+    /* Scratch buffer for temporary storage during file transfer */
+    char scratch[SCRATCH_BUFSIZE];
+};
 typedef struct{
 	bool IGN_status[5];
 } Rocket_status_t;
 
 esp_err_t Web_init(void);
+esp_err_t Web_off(void);
