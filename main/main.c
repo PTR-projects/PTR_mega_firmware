ff405c6c790f7bac2274b563bf054237a9e8b7f4
bartekM-pl <144720660+bartekM-pl@users.noreply.github.com>
2024-06-15 14:39:16 +0200

    IF-IF-IF tree fixed

--- main/main.c
+++ main/main.c
@@ -168,22 +168,31 @@ void task_kpptr_storage(void *pvParameter){
 
 	xLastWakeTime = xTaskGetTickCount ();
 	while(1){
-		vTaskDelayUntil(&xLastWakeTime, 2);	// Minimum 2 Ticks for 1 loop - avoid blocking Flash memory for too long
+		// Minimum 2 Ticks for 1 loop - avoid blocking Flash memory for too long
+		vTaskDelayUntil(&xLastWakeTime, 2);
 
-		if((FSD_getState() >= FLIGHTSTATE_ME_ACCELERATING) && (FSD_getState() < FLIGHTSTATE_SHUTDOWN)){
-			if(DM_getUsedPointerFromMainRB_wait(&DataPackage_ptr) == ESP_OK){	//wait max 100ms for new data
-				if(write_error_cnt < 1000){
-					if(Storage_writePacket((void*)DataPackage_ptr, sizeof(DataPackage_t)) != ESP_OK){
-						ESP_LOGE(TAG, "Storage task - packet write fail");
-						write_error_cnt++;
-					} else {
-						write_error_cnt = 0;	// Reset error counter if write successful
-					}
-				}
-				DM_returnUsedPointerToMainRB(&DataPackage_ptr);
-			} else {
-				ESP_LOGI(TAG, "Storage timeout");
-			}
+		// Skip if we are not flying yet
+		if((FSD_getState() < FLIGHTSTATE_ME_ACCELERATING) && (FSD_getState() >= FLIGHTSTATE_SHUTDOWN))
+				continue;
+
+		// Skip if timeout occured (max 100ms)
+		if(DM_getUsedPointerFromMainRB_wait(&DataPackage_ptr) != ESP_OK){
+			ESP_LOGI(TAG, "Storage timeout");
+			continue;
+		}
+
+		// Skip if too many write errors occured but free used pointer
+		if(write_error_cnt > 1000){
+			DM_returnUsedPointerToMainRB(&DataPackage_ptr);
+			continue;
+		}
+
+		// Try write data to Flash
+		if(Storage_writePacket((void*)DataPackage_ptr, sizeof(DataPackage_t)) != ESP_OK){
+			ESP_LOGE(TAG, "Storage task - packet write fail");
+			write_error_cnt++;
+		} else {
+			write_error_cnt = 0;	// Reset error counter if write successful
 		}
 	}
 }
