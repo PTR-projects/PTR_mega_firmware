d57d80e19f02d6cfaca4c7647ea03074e5d7375c
bartekM-pl <144720660+bartekM-pl@users.noreply.github.com>
2024-06-18 22:23:22 +0200

    Preparations for test flight!

--- components/FlightStateDetector/FlightStateDetector.c
+++ components/FlightStateDetector/FlightStateDetector.c
@@ -116,6 +116,10 @@ esp_err_t FSD_detect(uint64_t time_ms){
 		FlightState_LANDING(time_ms, currentState, ahrs);
 		break;
 
+	case FLIGHTSTATE_SHUTDOWN:
+		FSD_disarming();
+		break;
+
 	default:
 		ESP_LOGE(TAG, "Wrong state! Disarming!");
 		FSD_disarming();
@@ -264,7 +268,7 @@ static void FlightState_MAINSHUTE_FALL (uint64_t time_ms, FlightState_t * curren
 
 
 	//------ Warunki przejścia dalej ------
-	if (TIME_ELAPSED(stateChangeTime, time_ms, 10000) /* && (ahrs->pos.z < 200.0f) && (ahrs->vel.z > -1.0f)) */){
+	if (TIME_ELAPSED(stateChangeTime, time_ms, 10000) && (ahrs->velocityP > -0.5f)/* && (ahrs->pos.z < 200.0f) && (ahrs->vel.z > -1.0f)) */){
 		currentState->state = FLIGHTSTATE_LANDING;
 		currentState->state_ready = false;
 	}
@@ -278,7 +282,19 @@ static void FlightState_MAINSHUTE_FALL (uint64_t time_ms, FlightState_t * curren
 }
 
 static void FlightState_LANDING	(uint64_t time_ms, FlightState_t * currentState, AHRS_t * ahrs){
+	//------ Komendy wykonywane tylko raz ------
+	if(!(currentState->state_ready)) {
+		currentState->state_ready = true;
+		stateChangeTime = time_ms;
+	}
+
 	//------ Komendy wykonywane co pętlę ------
+
+	//------ Warunki przejścia dalej ------
+	if (TIME_ELAPSED(stateChangeTime, time_ms, 10000)){
+		currentState->state = FLIGHTSTATE_SHUTDOWN;
+		currentState->state_ready = false;
+	}
 
 	//------ Warunki wykrycia awarii ------
 	if(0 /* && (SysManager_checkCriticalError() != ESP_OK) */) {
